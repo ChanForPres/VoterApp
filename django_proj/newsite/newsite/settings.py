635d4b57f4561c505c897cfdcdf418047cca6784
mtchan <marcuschan999@gmail.com>
2017-09-04 15:26:35 -0800

    added choice,question models ; setup sqlite3 DB

--- django_proj/newsite/newsite/settings.py
+++ django_proj/newsite/newsite/settings.py
@@ -32,11 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'polls.apps.PollsConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
